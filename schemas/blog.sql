271f513ce690aa983de13511504fd8fc7a717bf0
Jake Barter <midg3tjake@live.co.uk>
2018-10-14 19:06:39 +0100

    ...

--- schemas/blog.sql
+++ schemas/blog.sql
@@ -1,5 +1,5 @@
 CREATE TABLE posts (
-    postid character varying(31); PRIMARY KEY,
+    postid character varying(31) PRIMARY KEY,
     title TEXT NOT NULL,
     postdate TIMESTAMP WITH TIME ZONE NOT NULL,
     editdate TIMESTAMP WITH TIME ZONE,
